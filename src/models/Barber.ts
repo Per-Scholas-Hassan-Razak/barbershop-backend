3a60758b27710dccd789e609ed3819ae9496398d
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-09-12 10:42:00 -0400

    user model created, linked to barber prep for auth

--- src/models/Barber.ts
+++ src/models/Barber.ts
@@ -1,17 +1,11 @@
 import mongoose from "mongoose";
 
 const barberSchema = new mongoose.Schema({
-    name:{type:String, required:true}, 
-    email:{
-        type:String,
-        required:true,
-        match:[/.+\@.+\..+/, 'Please fill a valid email address'] 
-    },
-    specialties:[String],
-    available:{type:Boolean, default:true},
-    createdAt:{type:Date, default:Date.now}
-})
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  specialties: [String],
+  available: { type: Boolean, default: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
-const Barber = mongoose.model('Barber', barberSchema)
+const Barber = mongoose.model("Barber", barberSchema);
 export default Barber;
-
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,16 @@
-import dotenv from "dotenv"
-dotenv.config()
-import connectDB from './config/db';
-import app from "./app/app"
-const PORT = process.env.PORT
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db";
+import app from "./app/app";
+const PORT = process.env.PORT;
 
 const env = process.env.NODE_ENV || "development";
 
-
-
-connectDB()
-.then((conn) => {
+connectDB().then((conn) => {
   app.listen(PORT, () => {
-    const connectionString = env === 'production' 
-                        ? console.log(`Server running at ${conn.connection.host}`)
-                        :console.log(`Server running at http://localhost:${PORT}`)
+    const connectionString =
+      env === "production"
+        ? console.log(`Production Server Running at: ${conn.connection.host}`)
+        : console.log(`Development Server Running at http://localhost:${PORT}`);
   });
-})
-
+});
