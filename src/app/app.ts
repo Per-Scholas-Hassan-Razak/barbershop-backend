52f48f8f2c71ca6681f1dc3189d91ca7187fa1fe
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-09-17 12:52:27 -0400

    cors policy updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,17 @@ import express from "express";
 import barberRoutes from "../routes/barberRoutes";
 import userRoutes from "../routes/userRoutes";
 import queueRoutes from "../routes/queueRoutes";
-
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend dev server
+    credentials: true, // allow cookies if you use them
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/v1/users", userRoutes);
